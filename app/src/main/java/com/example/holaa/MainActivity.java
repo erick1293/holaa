138a5041aea7f56eef0b1e3e43399c1e45440a38
erick1293 <erick.meneses1293@gmail.com>
2022-10-19 17:52:25 -0300

    Clase 1 Conectar activity

--- app/src/main/java/com/example/holaa/MainActivity.java
+++ app/src/main/java/com/example/holaa/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.SizeF;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,23 +15,22 @@ import java.security.Principal;
 
 public class MainActivity extends AppCompatActivity {
 
-Button btnlogin ; EditText edCorreo, edPass;
+Button btnlogin ; EditText edCorreo, edPass; TextView cCuenta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         llamarbtnlogin();
-
-
+        llamarCrearCuenta();
     }
+
     public void llamarbtnlogin(){
         btnlogin = (Button)  findViewById(R.id.btnlogin);
         btnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 edCorreo = (EditText) findViewById(R.id.etCorreo);
-
                 edPass = (EditText) findViewById(R.id.etPass);
                   String   correo = edCorreo.getText().toString();
                 String   pass = edPass.getText().toString();
@@ -43,7 +43,20 @@ Button btnlogin ; EditText edCorreo, edPass;
                     Toast.makeText(MainActivity.this, "no loguiaste", Toast.LENGTH_SHORT).show();
                 }
             }
-        });
+        });}
+
+        public  void llamarCrearCuenta() {
+            cCuenta = (TextView) findViewById(R.id.cCuenta);
+            cCuenta.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(MainActivity.this, "quiero crear una cuenta", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(MainActivity.this,crearCuenta.class);
+                    startActivity(i);
+
+                }
+            });
+
 
 
 
---
+++ app/src/main/java/com/example/holaa/crearCuenta.java
@@ -0,0 +1,31 @@
+package com.example.holaa;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.holaa.databinding.ActivityCrearCuentaBinding;
+
+public class crearCuenta extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_cuenta);
+
+
+
+    }
+
+
+}
