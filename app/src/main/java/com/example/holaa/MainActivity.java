0045871c261d231dfa181c111d4352fba4f37dd8
Alumnos Copiapó Laboratorio 412L <COP412L@inacap.cl>
2022-10-19 16:51:18 -0300

    Initial commit

---
+++ app/src/main/java/com/example/holaa/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.holaa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+Button btnlogin ; EditText edCorreo, edPass;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        llamarbtnlogin();
+
+
+    }
+    public void llamarbtnlogin(){
+        btnlogin = (Button)  findViewById(R.id.btnlogin);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                edCorreo = (EditText) findViewById(R.id.etCorreo);
+
+                edPass = (EditText) findViewById(R.id.etPass);
+                  String   correo = edCorreo.getText().toString();
+                String   pass = edPass.getText().toString();
+                if (correo.equals("admin.admin.cl") && pass.equals("admin")){
+                    Toast.makeText(MainActivity.this, "estoy loguiado", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "no loguiaste", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+    }
+}
