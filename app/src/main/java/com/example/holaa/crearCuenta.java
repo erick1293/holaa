ef6f8b4668a32b7ecc4619cd70c2ecdb6a6fc5a1
derick <erick.meneses1293@gmail.com>
2022-10-19 21:37:45 -0400

    trabajo en casa

--- app/src/main/java/com/example/holaa/crearCuenta.java
+++ app/src/main/java/com/example/holaa/crearCuenta.java
@@ -7,6 +7,7 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -18,12 +19,12 @@ import com.example.holaa.databinding.ActivityCrearCuentaBinding;
 public class crearCuenta extends AppCompatActivity {
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_cuenta);
-
-
+        Toast.makeText(this, "ya se creo la cuenta", Toast.LENGTH_SHORT).show();
 
     }
 
