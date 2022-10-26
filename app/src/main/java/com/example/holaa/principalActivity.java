4cbfa675f5934fe62f47da458c86b4e4ef2a25b5
erick1293 <erick.meneses1293@gmail.com>
2022-10-26 16:12:02 -0300

    c

--- app/src/main/java/com/example/holaa/principalActivity.java
+++ app/src/main/java/com/example/holaa/principalActivity.java
@@ -1,8 +1,13 @@
 package com.example.holaa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class principalActivity extends AppCompatActivity {
 
@@ -10,5 +15,32 @@ public class principalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.menuOp1:
+                Toast.makeText(this, "Opcion 1", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menuOp2:
+                getSupportFragmentManager().beginTransaction().replace(R.id.containerFragmento,fragment_alumnos.class,null).commit();
+                return true;
+            case R.id.menuOp3:
+                Toast.makeText(this, "Opcion 3", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
---
+++ app/src/main/java/com/example/holaa/toolbar.java
@@ -0,0 +1,14 @@
+package com.example.holaa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class toolbar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toolbar);
+    }
+}
