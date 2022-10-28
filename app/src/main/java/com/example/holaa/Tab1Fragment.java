5ac9789bcd27af3748d9b0ab8543644e9c68fbef
erick1293 <erick.meneses1293@gmail.com>
2022-10-28 10:53:26 -0300

    asd

---
+++ app/src/main/java/com/example/holaa/Tab1Fragment.java
@@ -0,0 +1,33 @@
+package com.example.holaa;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Tab1Fragment extends Fragment {
+
+
+
+    public Tab1Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tab1, container, false);
+    }
+}
--- app/src/main/java/com/example/holaa/principalActivity.java
+++ app/src/main/java/com/example/holaa/principalActivity.java
@@ -7,9 +7,13 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TableLayout;
 import android.widget.Toast;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class principalActivity extends AppCompatActivity {
+    TabLayout tablayaout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,36 @@ public class principalActivity extends AppCompatActivity {
         setContentView(R.layout.activity_principal);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
+        tablayaout =(TabLayout) findViewById(R.id.tabL);
+        tablayaout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int resultado =tab.getPosition();
+                switch (resultado){
+                    case 0 :
+                        getSupportFragmentManager().beginTransaction().replace(R.id.containerFragmento,Tab1Fragment.class,null).commit();
+                        break;
+                    case 1 :
+                        getSupportFragmentManager().beginTransaction().replace(R.id.containerFragmento,Tab2Fragment.class,null).commit();
+                        break;
+                    case 2 :
+                        getSupportFragmentManager().beginTransaction().replace(R.id.containerFragmento,Tab3Fragment.class,null).commit();
+                        break;
+
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 
     @Override
