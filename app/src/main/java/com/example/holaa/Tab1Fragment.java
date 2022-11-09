b8a185c37c46b68ac32e74836a41d60a28edccf7
erick1293 <erick.meneses1293@gmail.com>
2022-11-09 16:20:30 -0300

    dsad

--- app/src/main/java/com/example/holaa/Tab1Fragment.java
+++ app/src/main/java/com/example/holaa/Tab1Fragment.java
@@ -7,12 +7,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class Tab1Fragment extends Fragment {
 
-
-
+    EditText etNombre,etApellido,etEdad;
+    Button btnRegTrab;
     public Tab1Fragment() {
         // Required empty public constructor
     }
@@ -27,7 +29,7 @@ public class Tab1Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab1, container, false);
+
+         return inflater.inflate(R.layout.fragment_tab1, container, false);
     }
 }
--- app/src/main/java/com/example/holaa/Tab2Fragment.java
+++ app/src/main/java/com/example/holaa/Tab2Fragment.java
@@ -7,11 +7,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class Tab2Fragment extends Fragment {
 
-
+    EditText etNombre,etApellido,etEdad;
+    Button btnRegTrab;
     public Tab2Fragment() {
         // Required empty public constructor
     }
@@ -27,7 +31,20 @@ public class Tab2Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab2, container, false);
+        View v = inflater.inflate(R.layout.fragment_tab2, container, false);
+        etNombre = (EditText) v.findViewById(R.id.nombreTrabajador);
+        etApellido = (EditText)  v.findViewById(R.id.apellidoTrabajador);
+        etEdad = (EditText)  v.findViewById(R.id.edadTrabajador);
+        btnRegTrab = (Button)  v.findViewById(R.id.btnRegTrab);
+        btnRegTrab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //registrar ,enviar o base de datos
+                Toast.makeText(getContext(), "registrando", Toast.LENGTH_SHORT).show();
+                System.out.println("registrando!!");
+            }
+        });
+
+        return v;
     }
 }
